159fae600ddb2e31fe2ceb64feb6dfc7c1eff28d
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-09-10 23:45:39 +0530

    on Login User

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 
 
 //Register User 
@@ -18,8 +18,9 @@ try{
     return res.status(400).json({message: "User already exists"});  
 }
 
-// Hash password
 
+
+// Hash password
 const hashedPassword = await bcrypt.hash(password, 10);
 
 // Create new User 
@@ -53,15 +54,44 @@ const loginUser = async (req, res)=>{
 
   console.log("Login this page");
   try {
-    const existingUser = await User.findOne({email: req.body.email});
 
-    if(!existingUser){
+const {email, password} = req.body;
+
+
+  // 1. User ko email se find karo
+    const user  = await User.findOne({email});
+
+    if(!user){
       res.status(400).json({message: "User does not exist"});
     }
-     
 
+
+     // Password check
+     const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+     if(!isPasswordCorrect){
+    return res.status(400).json({message: "Invalid email or password"});
+     }
+      
+// JWT token create
+const token = jwt.sign({
+  userId: user._id
+},
+process.env.JWT_SECRET, {
+  expiresIn: "7d"
+}
+);
     
-
+// Response 
+res.status(200).json({
+  message: "Login successful",
+  token,
+  user: {
+    id: user._id,
+    name: user.name,
+    email: user.email
+  }
+})
     
   } catch (error) {
     console.log("Login Error:", error);
@@ -70,6 +100,7 @@ const loginUser = async (req, res)=>{
     
   }
 }
+
 
 module.exports = {
   registerUser, loginUser
